05cb3f2dcd657d343ab326d1b49dc2987b2676e5
mohd-afeef-badri <afeef.badri@gmail.com>
2025-08-04 09:28:45 +0200

    [feat] msh 4.1 format for sequential

--- src/mesher/topIIvol_Mesher.cpp
+++ src/mesher/topIIvol_Mesher.cpp
@@ -412,13 +412,270 @@ int main(int argc, char *argv[])
 
 //-----------------------------------------------------------------------------------//
 //-----------------------------------------------------------------------------------//
-//---- Writing mesh in Gmsh's .msh format -----
+//---- Writing mesh in Gmsh's .msh format 4.1 / 2.2 -----
 //-----------------------------------------------------------------------------------//
 //-----------------------------------------------------------------------------------//
 
+        else if (meshtype == "msh4")
+        {
+            cout << "   Meshing the topology in Gmsh's *.msh 4.1 format\n";
+
+//--------------------------------------//
+//---- Header for msh4.1 -----
+//--------------------------------------//
+
+            std::fprintf(w2f, "$MeshFormat\n4.1 0 8\n$EndMeshFormat\n");
+
+            // Physical Names section
+            std::fprintf(w2f, "$PhysicalNames\n");
+            std::fprintf(w2f, "7\n");              // number of physical groups
+            std::fprintf(w2f, "2 1 \"left\"\n");   // X-min surface (physical tag 1)
+            std::fprintf(w2f, "2 2 \"front\"\n");  // Y-min surface (physical tag 2)
+            std::fprintf(w2f, "2 3 \"bottom\"\n"); // Z-min surface (physical tag 3)
+            std::fprintf(w2f, "2 4 \"right\"\n");  // X-max surface (physical tag 4)
+            std::fprintf(w2f, "2 5 \"back\"\n");   // Y-max surface (physical tag 5)
+            std::fprintf(w2f, "2 6 \"top\"\n");    // Z-max surface (physical tag 6)
+            std::fprintf(w2f, "3 7 \"volume\"\n"); // Volume (physical tag 7)
+            std::fprintf(w2f, "$EndPhysicalNames\n");
+
+            // Entities section (required in MSH 4.1)
+            std::fprintf(w2f, "$Entities\n");
+            // Format: numPointEntities numCurveEntities numSurfaceEntities numVolumeEntities
+            std::fprintf(w2f, "0 0 6 1\n");
+
+            // Define 6 surfaces (boundary faces)
+            // Format: surfaceTag minX minY minZ maxX maxY maxZ numBoundingCurves
+            std::fprintf(w2f, "1 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 1 0\n"); // X-min surface
+            std::fprintf(w2f, "2 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 2 0\n"); // Y-min surface
+            std::fprintf(w2f, "3 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 3 0\n"); // Z-min surface
+            std::fprintf(w2f, "4 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 4 0\n"); // X-max surface
+            std::fprintf(w2f, "5 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 5 0\n"); // Y-max surface
+            std::fprintf(w2f, "6 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 6 0\n"); // Z-max surface
+
+            // Define 1 volume
+            // Format: volumeTag minX minY minZ maxX maxY maxZ numBoundingSurfaces surfaceTag1 surfaceTag2 ...
+            std::fprintf(w2f, "1 -1e99 -1e99 -1e99 1e99 1e99 1e99 1 7 0\n");
+
+            std::fprintf(w2f, "$EndEntities\n");
+
+            // Nodes section
+            std::fprintf(w2f, "$Nodes\n");
+            std::fprintf(w2f, "1 %d 1 %d\n", NPnt, NPnt); // numEntityBlocks numNodes minNodeTag maxNodeTag
+            std::fprintf(w2f, "3 1 0 %d\n", NPnt);        // entityDim entityTag parametric numNodesInBlock
+
+            // Node tags
+            for (int i = 1; i <= NPnt; i++)
+            {
+                std::fprintf(w2f, "%d\n", i);
+            }
+
+            int counter1 = 1;
+
+            cout << "   Generating points....";
+
+            for (int i = 0; i < pntx * pnty; i++)
+            {
+                filePoints += fscanf(rf, "%lf", &xx);
+                filePoints += fscanf(rf, "%lf", &yy);
+                filePoints += fscanf(rf, "%lf", &zz);
+
+                std::fprintf(w2f, "%lf %lf %lf\n", xx, yy, zz);
+
+                counter1++;
+                zznew = zz;
+                delz = (zmax - zz) / PzM1;
+                for (int j = 0; j < PzM1; j++)
+                {
+                    zznew = zznew + delz;
+                    std::fprintf(w2f, "%lf %lf %lf\n", xx, yy, zznew);
+                    counter1++;
+                }
+            }
+
+            if (filePoints != pntx * pnty * 3)
+            {
+                cout << "ERROR! Something is not right \n"
+                     << filePoints * 3 << " Vertices read \n"
+                     << pntx * pnty << " Vertices expected \n";
+                exit(111);
+            }
+            filePoints = 0;
+
+            std::fclose(rf);
+            std::fprintf(w2f, "$EndNodes\n");
+            cout << "   Done\n";
+
+//-----------------------------------------------------------------------------------//
+//---- Generating Elements (Triangles and Tetrahedra) -----
+//-----------------------------------------------------------------------------------//
+
+            cout << "   Generating Elements....";
+            std::fprintf(w2f, "$Elements\n");
+
+            // Calculate number of element blocks (6 surface blocks + 1 volume block)
+            int numElementBlocks = 7;
+            std::fprintf(w2f, "%d %d 1 %d\n", numElementBlocks, NTet + NTri, NTet + NTri);
+
+            counter1 = 1;
+
+            //---------------------------------X-MIN-PLANE (Surface 1 - "left")----------------------------------//
+            int numTriXMin = 2 * PyM1 * PzM1;
+            std::fprintf(w2f, "2 1 2 %d\n", numTriXMin); // entityDim entityTag elementType numElementsInBlock
+
+            for (int i = 0; i < PyM1; i++)
+            {
+                for (int j = 0; j < PzM1; j++)
+                {
+                    IJK = i * Pxz + j + 1;
+                    IJKp1 = IJK + 1;
+                    Ip1JK = IJK + Pxz;
+                    Ip1JKp1 = Ip1JK + 1;
+
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1, IJKp1, IJK, Ip1JK);
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1 + 1, Ip1JKp1, IJKp1, Ip1JK);
+                    counter1 += 2;
+                }
+            }
+
+            //---------------------------------Y-MIN-PLANE (Surface 2 - "front")----------------------------------//
+            int numTriYMin = 2 * PxM1 * PzM1;
+            std::fprintf(w2f, "2 2 2 %d\n", numTriYMin);
+
+            for (int i = 0; i < PxM1; i++)
+            {
+                for (int j = 0; j < PzM1; j++)
+                {
+                    IJK = i * pntz + j + 1;
+                    IJKp1 = IJK + 1;
+                    IJp1K = IJK + pntz;
+                    IJp1Kp1 = IJp1K + 1;
+
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1, IJK, IJKp1, IJp1K);
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1 + 1, IJKp1, IJp1Kp1, IJp1K);
+                    counter1 += 2;
+                }
+            }
+
+            //---------------------------------Z-MIN-PLANE (Surface 3 - "bottom")----------------------------------//
+            int numTriZMin = 2 * PyM1 * PxM1;
+            std::fprintf(w2f, "2 3 2 %d\n", numTriZMin);
+
+            for (int i = 0; i < PyM1; i++)
+            {
+                for (int j = 0; j < PxM1; j++)
+                {
+                    IJK = i * Pxz + j * pntz + 1;
+                    Ip1JK = IJK + Pxz;
+                    IJp1K = IJK + pntz;
+                    Ip1Jp1K = Ip1JK + pntz;
+
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1, IJK, IJp1K, Ip1Jp1K);
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1 + 1, Ip1JK, IJK, Ip1Jp1K);
+                    counter1 += 2;
+                }
+            }
+
+            //---------------------------------X-MAX-PLANE (Surface 4 - "right")----------------------------------//
+            int numTriXMax = 2 * (pnty - 1) * PzM1;
+            std::fprintf(w2f, "2 4 2 %d\n", numTriXMax);
+
+            for (int i = 0; i < pnty - 1; i++)
+            {
+                for (int j = 0; j < PzM1; j++)
+                {
+                    IJK = i * Pxz + j + 1 + PxM1 * pntz;
+                    IJKp1 = IJK + 1;
+                    Ip1JK = IJK + Pxz;
+                    Ip1JKp1 = Ip1JK + 1;
+
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1, IJK, IJKp1, Ip1JK);
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1 + 1, IJKp1, Ip1JKp1, Ip1JK);
+                    counter1 += 2;
+                }
+            }
+
+            //---------------------------------Y-MAX-PLANE (Surface 5 - "back")----------------------------------//
+            int numTriYMax = 2 * PxM1 * PzM1;
+            std::fprintf(w2f, "2 5 2 %d\n", numTriYMax);
+
+            for (int i = 0; i < PxM1; i++)
+            {
+                for (int j = 0; j < PzM1; j++)
+                {
+                    IJK = i * pntz + j + 1 + Pxz * PyM1;
+                    IJKp1 = IJK + 1;
+                    IJp1K = IJK + pntz;
+                    IJp1Kp1 = IJp1K + 1;
+
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1, IJKp1, IJK, IJp1K);
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1 + 1, IJp1Kp1, IJKp1, IJp1K);
+                    counter1 += 2;
+                }
+            }
+
+            //---------------------------------Z-MAX-PLANE (Surface 6 - "top")----------------------------------//
+            int numTriZMax = 2 * PyM1 * PxM1;
+            std::fprintf(w2f, "2 6 2 %d\n", numTriZMax);
+
+            for (int i = 0; i < PyM1; i++)
+            {
+                for (int j = 0; j < PxM1; j++)
+                {
+                    IJK = i * Pxz + j * pntz + 1 + PzM1;
+                    Ip1JK = IJK + Pxz;
+                    IJp1K = IJK + pntz;
+                    Ip1Jp1K = Ip1JK + pntz;
+
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1, IJp1K, IJK, Ip1Jp1K);
+                    std::fprintf(w2f, "%d %d %d %d\n", counter1 + 1, IJK, Ip1JK, Ip1Jp1K);
+                    counter1 += 2;
+                }
+            }
+
+//-----------------------------------------------------------------------------------//
+//---- Generating Tetrahedra (Volume 1 - "volume") -----
+//-----------------------------------------------------------------------------------//
+
+            cout << "Done  \n"
+                 << "   Generating Tetrahedrals....";
+
+            // Volume elements
+            std::fprintf(w2f, "3 1 4 %d\n", NTet); // entityDim entityTag elementType(4=tetrahedra) numElementsInBlock
+
+            for (int j = 0; j < PyM1; j++)
+            {
+                for (int i = 0; i < PxM1; i++)
+                {
+                    for (int k = 1; k <= PzM1; k++)
+                    {
+                        IJK = i * pntz + j * Pxz + k;
+                        Ip1JK = IJK + Pxz;
+                        IJp1K = IJK + pntz;
+                        Ip1Jp1K = IJK + Pxz + pntz;
+                        IJKp1 = IJK + 1;
+                        Ip1JKp1 = Ip1JK + 1;
+                        IJp1Kp1 = IJp1K + 1;
+                        Ip1Jp1Kp1 = Ip1Jp1K + 1;
+
+                        std::fprintf(w2f, "%d %d %d %d %d\n", counter1, IJK, IJKp1, IJp1K, Ip1Jp1K);
+                        std::fprintf(w2f, "%d %d %d %d %d\n", counter1 + 1, IJKp1, IJK, Ip1JK, Ip1Jp1K);
+                        std::fprintf(w2f, "%d %d %d %d %d\n", counter1 + 2, Ip1JKp1, IJKp1, Ip1JK, Ip1Jp1K);
+                        std::fprintf(w2f, "%d %d %d %d %d\n", counter1 + 3, IJKp1, Ip1JKp1, Ip1Jp1Kp1, Ip1Jp1K);
+                        std::fprintf(w2f, "%d %d %d %d %d\n", counter1 + 4, IJp1Kp1, IJKp1, Ip1Jp1Kp1, Ip1Jp1K);
+                        std::fprintf(w2f, "%d %d %d %d %d\n", counter1 + 5, IJKp1, IJp1Kp1, IJp1K, Ip1Jp1K);
+
+                        counter1 += 6;
+                    }
+                }
+            }
+
+            std::fprintf(w2f, "$EndElements\n");
+            cout << "Done\n";
+        }
+
     else if (meshtype == "msh")
         {
-            cout << "   Meshing the topology in Gmsh's *.msh format\n";
+            cout << "   Meshing the topology in Gmsh's *.msh 2.2 format\n";
 
 //--------------------------------------//
 //---- Header for msh2 -----
@@ -935,7 +1192,7 @@ int main(int argc, char *argv[])
     else
         {
             cout << " *=============================================================*\n"
-                 << " * ERROR: --mesh input is wrong only msh or mesh is accepted   *\n"
+                 << " * ERROR: --mesh input is wrong only msh, msh4, or mesh is accepted   *\n"
                  << " *=============================================================*\n\n";
         }
 delete inputfile;
